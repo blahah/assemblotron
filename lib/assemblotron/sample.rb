d7c6024356783a43ddefa972d5b6273b1a24efc9
Richard Smith <rds45@cam.ac.uk>
2014-04-22 12:26:24 +0100

    ignore gcc warnings for rubyinline (workaround stupid gcc defaults on some systems)

--- lib/assemblotron/sample.rb
+++ lib/assemblotron/sample.rb
@@ -11,126 +11,118 @@ module Assemblotron
     end
 
     inline :C do |builder|
-      builder.include '<stdio.h>'
-      builder.include '<strings.h>'
-      builder.c <<SRC
-        void subsampleC(VALUE n, VALUE seed, VALUE left, VALUE right, VALUE leftout, VALUE rightout) {
-          char * filename_left;
-          char * filename_right;
-          char * outname_left;
-          char * outname_right;
-          char * line_l1 = NULL;
-          char * line_l2 = NULL;
-          char * line_l3 = NULL;
-          char * line_l4 = NULL;
-          char * line_r1 = NULL;
-          char * line_r2 = NULL;
-          char * line_r3 = NULL;
-          char * line_r4 = NULL;
-          size_t len = 0;
-          ssize_t read_l;
-          ssize_t read_r;
-          unsigned nc, i;
-          float a, r;
-          unsigned long count;
-          FILE *lfp;
-          FILE *rfp;
-          FILE *lout;
-          FILE *rout;
-          char *res_l = NULL;
-          char *res_r = NULL;
+    builder.add_compile_flags %q(-w)
+    builder.include '<stdio.h>'
+    builder.include '<strings.h>'
+    builder.c <<SRC
+      void subsample(VALUE n, VALUE seed, VALUE left, VALUE right, VALUE leftout, VALUE rightout) {
+        char * filename_left;
+        char * filename_right;
+        char * outname_left;
+        char * outname_right;
+        char * line_l1 = NULL;
+        char * line_l2 = NULL;
+        char * line_l3 = NULL;
+        char * line_l4 = NULL;
+        char * line_r1 = NULL;
+        char * line_r2 = NULL;
+        char * line_r3 = NULL;
+        char * line_r4 = NULL;
+        size_t len = 0;
+        ssize_t read_l;
+        ssize_t read_r;
+        unsigned nc,i,seedc;
+        float a,r;
+        unsigned long count;
+        FILE *lfp;
+        FILE *rfp;
 
-          nc = NUM2INT(n);
-          res_l = (char*) realloc(res_l, nc * sizeof(char));
-          res_r = (char*) realloc(res_r, nc * sizeof(char));
+        nc = NUM2INT(n);
+        seedc = NUM2INT(seed);
+        const char *res_l[nc];
+        const char *res_r[nc];
+ 
+        srand(seedc);
+        filename_left = StringValueCStr(left);
+        filename_right = StringValueCStr(right);
+        outname_left = StringValueCStr(leftout);
+        outname_right = StringValueCStr(rightout);
 
-          seedc = NUM2INT(seed);
-          srand(seedc);
+        lfp = fopen(filename_left, "r");
+        rfp = fopen(filename_right, "r");
 
-          filename_left = StringValueCStr(left);
-          filename_right = StringValueCStr(right);
-          outname_left = StringValueCStr(leftout);
-          outname_right = StringValueCStr(rightout);
-
-          lfp = fopen(filename_left, "r");
-          rfp = fopen(filename_right, "r");
-
-          if (lfp == NULL) {
-            fprintf(stderr, "Cant open file!\\n");
-            exit(1);
-          }
-          if (rfp == NULL) {
-            fprintf(stderr, "Cant open file!\\n");
-            exit(1);
-          }
-
-          count = 1;
-          while ((read_l = getline(&line_l1, &len, lfp)) != -1) {
-            char * str_l = (char *) malloc(400);
-            char * str_r = (char *) malloc(400);
-
-            read_l += getline(&line_l2, &len, lfp);
-            read_l += getline(&line_l3, &len, lfp);
-            read_l += getline(&line_l4, &len, lfp);
-            read_r =  getline(&line_r1, &len, rfp);
-            read_r += getline(&line_r2, &len, rfp);
-            read_r += getline(&line_r3, &len, rfp);
-            read_r += getline(&line_r4, &len, rfp);
-            
-            strcpy (str_l, line_l1);
-            strcat (str_l, line_l2);
-            strcat (str_l, line_l3);
-            strcat (str_l, line_l4);
-
-            strcpy (str_r, line_r1);
-            strcat (str_r, line_r2);
-            strcat (str_r, line_r3);
-            strcat (str_r, line_r4);
-
-            a = 1.0;
-            if (count <= nc) {
-              res_l[count-1] = str_l;
-              res_r[count-1] = str_r;
-            } else {
-              r = ((float)rand()/(float)(RAND_MAX)) * a;
-              if (r < (float)nc/(float)count) {
-                i = rand() % nc;
-                if (i < 0 || i >= nc) {
-                  printf("ERROR, index out of bounds exception");
-                }
-                res_l[i] = str_l;
-                res_r[i] = str_r;
-              } else {
-                free(str_l);
-                free(str_r);
-              }
-            }
-            ++count;
-          }
-
-          fclose(lfp);
-          fclose(rfp);
-
-          lout = fopen(outname_left, "w");
-          rout = fopen(outname_right, "w");
-
-          if (lout == NULL) {
-              printf("Error opening left file for writing\\n");
-              exit(1);
-          }
-          if (rout == NULL) {
-              printf("Error opening right file for writing\\n");
-              exit(1);
-          }
-
-          for(i=0; i<nc; i++) {
-            fprintf(lout,"%s",res_l[i]);
-            fprintf(rout,"%s",res_r[i]);
-          }
-
-          fclose(lout);
-          fclose(rout);
+        if (lfp == NULL) {
+          fprintf(stderr, "Cant open file!\\n");
+          exit(1);
         }
+        if (rfp == NULL) {
+          fprintf(stderr, "Cant open file!\\n");
+          exit(1);
+        }
+
+        count=1;
+        while ((read_l = getline(&line_l1, &len, lfp)) != -1) {
+          read_l += getline(&line_l2, &len, lfp);
+          read_l += getline(&line_l3, &len, lfp);
+          read_l += getline(&line_l4, &len, lfp);
+          read_r =  getline(&line_r1, &len, rfp);
+          read_r += getline(&line_r2, &len, rfp);
+          read_r += getline(&line_r3, &len, rfp);
+          read_r += getline(&line_r4, &len, rfp);
+          
+          char * str_l = (char *) malloc(400);
+          char * str_r = (char *) malloc(400);
+          strcpy (str_l, line_l1);
+          strcat (str_l, line_l2);
+          strcat (str_l, line_l3);
+          strcat (str_l, line_l4);
+
+          strcpy (str_r, line_r1);
+          strcat (str_r, line_r2);
+          strcat (str_r, line_r3);
+          strcat (str_r, line_r4);
+
+          a = 1.0;
+          if (count <= nc) {
+            res_l[count-1] = str_l;
+            res_r[count-1] = str_r;
+          } else {
+            r = ((float)rand()/(float)(RAND_MAX)) * a;
+            if (r < (float)nc/(float)count) {
+              i = rand() % nc;
+              res_l[i] = str_l;
+              res_r[i] = str_r;
+            } else {
+              free(str_l);
+              free(str_r);
+            }
+          }
+          ++count;
+        }
+
+        fclose(lfp);
+        fclose(rfp);
+
+        FILE *lout = fopen(outname_left, "w");
+        FILE *rout = fopen(outname_right, "w");
+
+        if (lout == NULL) {
+            printf("Error opening left file for writing\\n");
+            exit(1);
+        }
+        if (rout == NULL) {
+            printf("Error opening right file for writing\\n");
+            exit(1);
+        }
+
+        for(i=0; i<nc; i++) {
+          fprintf(lout,"%s",res_l[i]);
+          fprintf(rout,"%s",res_r[i]);
+        }
+
+        fclose(lout);
+        fclose(rout);
+      }
 SRC
     end
 
@@ -144,7 +136,7 @@ SRC
       rdir = File.dirname(@right)
       routfile = File.join(rdir, "subset.#{File.basename @right}")
 
-      subsampleC(n,seed, @left, @right, loutfile, routfile)
+      subsampleC(n, seed, @left, @right, loutfile, routfile)
 
       [loutfile, routfile]
     end
